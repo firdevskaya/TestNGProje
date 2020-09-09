a4d1a8198d2a524d655f97e0e8bcd2de95bdec48
hamza <hyilmaz.techproed@gmail.com>
2020-09-09 23:19:56 +0300

    day17 ders sonu

--- src/test/java/com/techproed/utilities/TestBase.java
+++ src/test/java/com/techproed/utilities/TestBase.java
@@ -1,6 +1,8 @@
 package com.techproed.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -27,5 +29,6 @@ public abstract class TestBase {
     public void tearDown(){
         //driver.quit();
     }
+
 
 }
